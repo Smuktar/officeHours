3eb0555f475265787f2b89daf029aae73e0f6ce6
Selam Muktar <mselamm1@gmail.com>
2026-02-26 17:18:37 -0500

    starting a rock, paper, scissors game

---
+++ rps.py
@@ -0,0 +1,6 @@
+# create a rock, paper, scissor game
+# 3 options to choose from
+choice = ["rock", "paper", "scissors"]
+# need to be able to take a users input so they can select
+# need to give random choice to computer
+# create what to do depending on different outcomes
